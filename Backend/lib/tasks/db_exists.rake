17bd65ad7227706b3578b5f9eceb59d01926ce98
Ernest Badu <ernxst@outlook.com>
2021-12-08 21:19:44 +0000

    Only seed database if it is empty

---
+++ Backend/lib/tasks/db_exists.rake
@@ -0,0 +1,18 @@
+namespace :db do
+  desc "Checks to see if the database exists"
+  task :exists do
+    begin
+      Rake::Task['environment'].invoke
+      ActiveRecord::Base.connection
+      if Media::Texture.all == 0
+        exit 1
+      end
+    rescue
+      puts '== Database does not exist, will seed after creation =='
+      exit 1
+    else
+      puts '== Database exists, will not seed =='
+      exit 0
+    end
+  end
+end
--- Backend/tools/entrypoint.sh
+++ Backend/tools/entrypoint.sh
@@ -7,18 +7,24 @@ echo "== Removing pid files =="
 rm -f tmp/pids/server.pid
 
 if [[ "$RAILS_ENV" != "production" ]]; then
-  echo "== Waiting for Database"
-  while ! pg_isready -h db -p 5432 -q -U ${DB_USER}; do
-    echo >&2 "Postgres DB is unavailable - waiting"
+  echo "== Waiting for Database =="
+  while ! pg_isready -h db -p 5432 -q -U "${DB_USER}"; do
+    echo >&2 "    > Postgres DB is unavailable - waiting"
     sleep 1
   done
 fi
 
+echo "== Database Ready =="
+bundle exec rake db:exists || exists=false
+
 echo "== Preparing Database =="
 bundle exec rake db:prepare
 
-echo "== Seeding Database =="
-bundle exec rake db:seed
+if [[ "$exists" == "false" ]]; then
+  echo "== Seeding Database =="
+  bundle exec rake db:seed
+  echo "== Database Seeded =="
+fi
 
 echo "== Running App =="
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
