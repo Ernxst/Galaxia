88d1e180887e46c3d11df6283cdefe138645e611
Ernest Badu <ernxst@outlook.com>
2022-03-06 14:21:17 +0000

    Update dev backend API url

--- Frontend/src/services/api.constants.ts
+++ Frontend/src/services/api.constants.ts
@@ -1,7 +1,7 @@
 const PORT = 8080;
 let domain = process.env.NODE_ENV === "production"
   ? "https://galaxia-app.herokuapp.com"
-  : `http://localhost:${PORT}`;
+  : `http://192.168.1.140:${PORT}`;
 export const BASE_URL = `${domain}/api/v1`;
 export const RESPONSE_TIMEOUT = 20_000;
 
